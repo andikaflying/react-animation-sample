985d6e0e920655f127d0b65f4b5992e8e0b221fb
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-01-03 12:50:27 +0700

    init
    init

---
+++ src/components/Component2.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {Spring} from 'react-spring/renderprops';
+
+export default class Component2 extends Component {
+    render() {
+        return (
+            <Spring
+                from={{opacity : 0}}
+                to={{opacity: 1}}
+                config={{ delay: 1000, duration: 1000 }}
+            >
+                {props => (
+                        <div style={props}>
+                            <div style={c2Style}>
+                                <h1> Component 2</h1>
+                                <p> Loren ipsum dolor sit, amet consectutur adipsicing elit. 
+                                    Possimus nobsi adipsci um minima deserunt at porro. 
+                                    Possimus nobsi adipsci um minima deserunt at porro.
+                                    Possimus nobsi adipsci um minima deserunt at porro.
+                                    Possimus nobsi adipsci um minima deserunt at porro.
+                                </p>
+                                <button style={btn} onClick={this.props.toggle}>Toggle Component 3</button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Spring>
+        )
+    }
+}
+
+const c2Style = {
+    background: 'slateblue',
+    color: 'white',
+    padding: '1.5rem'
+}
+
+const btn = {
+    background: '#333',
+    color: '#fff',
+    padding: '1rem 2rem',
+    border: 'none',
+    textTransform: 'uppercase',
+    margin: '15px 0'
+}
---
+++ src/components/Component3.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Component3() {
+    return (
+        <div style={c3Style}>
+            <h1> Component 3 </h1>
+            <p> Loren ipsum dolor sit, amet consectutur adipsicing elit. 
+                Possimus nobsi adipsci um minima deserunt at porro. 
+                Possimus nobsi adipsci um minima deserunt at porro.
+                Possimus nobsi adipsci um minima deserunt at porro.
+                Possimus nobsi adipsci um minima deserunt at porro.
+            </p>
+        </div>
+    )
+}
+
+const c3Style = {
+    background: 'skyblue',
+    color: 'white',
+    padding: '1.5rem 1.5rem 5rem 1.5rem'
+}
